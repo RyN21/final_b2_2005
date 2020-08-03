616e834be7d7a13d177f07501e3f0e89a021cb2d
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-03 09:22:05 -0600

    Add ariline class. Clear coverage

--- spec/flights/flights_show_spec.rb
+++ spec/flights/flights_show_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Flights Show Page" do
   before :each do
-    @frontier = Airline.create(name: “Frontier”)
-    @flight_1 = @frontier.flights.create(number: “1727”, date: “08/03/20”, departure_city: “Denver”, arrival_city: “Reno”)
-    @joe = Passenger.create(name: "Joe", age: 7)
-    @ron = Passenger.create(name: "Ron", age: 7)
-    @olivia = Passenger.create(name: "Olivia", age: 7)
-    @reija = Passenger.create(name: "Reija", age: 7)
+    @frontier = Airline.create(name: "Frontier")
+    @flight_1 = @frontier.flights.create(number: "1727", date: "08/03/20", departure_city: "Denver", arrival_city: "Reno")
+    @joe      = Passenger.create(name: "Joe", age: 7)
+    @ron      = Passenger.create(name: "Ron", age: 7)
+    @olivia   = Passenger.create(name: "Olivia", age: 7)
+    @reija    = Passenger.create(name: "Reija", age: 7)
 
-    @fligh_passengers.create(flight: @flight_1, passenger: @joe)
-    @fligh_passengers.create(flight: @flight_1, passenger: @ron)
-    @fligh_passengers.create(flight: @flight_1, passenger: @olivia)
-    @fligh_passengers.create(flight: @flight_1, passenger: @reija)
+    @fligh_passengers1 = FlightPassengers.create(flight: @flight_1, passenger: @joe)
+    @fligh_passengers2 = FlightPassengers.create(flight: @flight_1, passenger: @ron)
+    @fligh_passengers3 = FlightPassengers.create(flight: @flight_1, passenger: @olivia)
+    @fligh_passengers4 = FlightPassengers.create(flight: @flight_1, passenger: @reija)
 
     visit "/flights/#{@flight_1.id}"
   end
@@ -36,19 +36,3 @@ RSpec.describe "Flights Show Page" do
     expect(page).to have_content(@reija.name)
   end
 end
-
-
-
-
-
-
-
-I see all of that flights information including:
-  - number
-  - date
-  - time
-  - departure city
-  - arrival city
-And I see the name of the airline this flight belongs
-And I see the names of all of the passengers on this flight
-```
