266139e41d5b0403bf2673269353ce5a65cd1aef
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-03 09:30:23 -0600

    Add database relation to flights to airline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803152719) do
+ActiveRecord::Schema.define(version: 20200803152857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20200803152719) do
     t.string "arrival_city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "airline_id"
+    t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
+  add_foreign_key "flights", "airlines"
 end
